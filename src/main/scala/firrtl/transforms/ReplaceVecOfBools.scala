a5d597350cd0893367b4252b44ffc778c39c814b
Abert Chen <albert.chen@sifive.com>
2018-06-26 12:05:36 -0700

    working maybe

--- src/main/scala/firrtl/transforms/ReplaceVecOfBools.scala
+++ src/main/scala/firrtl/transforms/ReplaceVecOfBools.scala
@@ -10,24 +10,23 @@ import scala.collection.mutable
 
 object ReplaceVecOfBools {
 
-  private val replacedVecs = new mutable.HashMap[String, Expression]()
+  private val candidates = new mutable.HashMap[String, (Expression, Boolean)]()
   private val inputs = new mutable.HashSet[String]()
 
   private val fieldDelimeter = '.'
-  private val indexField = "[]"
 
   private def replaceAndRegister(name: String,
                                  isInput: Boolean = true,
                                  hasDirection: Boolean = false)(tpe: Type): Type = tpe match {
     case VectorType(BoolType, size) if  size > 0 && ((size & (size - 1)) == 0) =>
-      replacedVecs.put(name, UIntLiteral(0, IntWidth(size)))
+      candidates.put(name, (UIntLiteral(0, IntWidth(size)), false))
       if (hasDirection && isInput) {
         inputs.add(name)
-        replacedVecs.put(name, keyToExpr(name).mapType(_ => UIntType(IntWidth(size))))
+        candidates.put(name, (keyToExpr(name).mapType(_ => UIntType(IntWidth(size))), false))
       }
       UIntType(IntWidth(size))
 
-    case BundleType(fields) => BundleType(fields.map { case field =>
+    case BundleType(fields) => BundleType(fields.map { field =>
       field.copy(tpe = replaceAndRegister(name + fieldDelimeter + field.name,
         isInput ^ field.flip == Flip,
         hasDirection)(field.tpe))
@@ -35,9 +34,11 @@ object ReplaceVecOfBools {
     case other => other
   }
 
-  private def isReplaced(expr: Expression): Boolean = replacedVecs.contains(expr.serialize)
-  private def getDefault(expr: Expression): Expression = replacedVecs(expr.serialize)
-  private def setDefault(expr: Expression, default: Expression): Unit = replacedVecs.put(expr.serialize, default)
+  private def isReplaced(expr: Expression): Boolean = candidates.contains(expr.serialize)
+  private def getDefault(expr: Expression): Expression = candidates(expr.serialize)._1
+  private def setDefault(expr: Expression, default: Expression): Unit = {
+    candidates.put(expr.serialize, (default, true))
+  }
 
   private def assignVec(info: Info, namespace: Namespace, origExpr: Expression, value: Expression): DefNode = {
     val default = getDefault(origExpr)
@@ -57,12 +58,46 @@ object ReplaceVecOfBools {
       DoPrim(PrimOps.Cat, Seq(SubIndex(value, i, BoolType), expr), Seq.empty, UIntType(IntWidth(i + 1))))
   }
 
+  private def replaceAccess(namespace: Namespace,
+                            info: Info,
+                            vec: Expression,
+                            default: Expression,
+                            value: Expression,
+                            index: Expression,
+                            tpe: Type): Statement = {
+    val shiftedValue = DoPrim(PrimOps.Dshl, Seq(value, index), Seq.empty, default.tpe)
+
+    val UIntType(IntWidth(width)) = default.tpe
+    val oneHot = DoPrim(PrimOps.Dshl, Seq(UIntLiteral(1, IntWidth(width)), index), Seq.empty, tpe)
+    val mask = DoPrim(PrimOps.Not, Seq(oneHot), Seq.empty, tpe)
+    val maskedDefault = DoPrim(PrimOps.And, Seq(default, mask), Seq.empty, tpe)
+
+    assignVec(info, namespace, vec, DoPrim(PrimOps.Or, Seq(maskedDefault, shiftedValue), Seq.empty, tpe))
+  }
+
+  private def replaceIndex(namespace: Namespace,
+                           info: Info,
+                           vec: Expression,
+                           index: Int,
+                           default: Expression,
+                           value: Expression,
+                           tpe: Type): Statement = {
+    val shiftedValue = DoPrim(PrimOps.Shl, Seq(value), Seq(index), default.tpe)
+
+    val UIntType(IntWidth(width)) = default.tpe
+    val oneHot = DoPrim(PrimOps.Shl, Seq(UIntLiteral(1, IntWidth(width))), Seq(index), tpe)
+    val mask = DoPrim(PrimOps.Not, Seq(oneHot), Seq.empty, tpe)
+    val maskedDefault = DoPrim(PrimOps.And, Seq(default, mask), Seq.empty, tpe)
+
+    assignVec(info, namespace, vec, DoPrim(PrimOps.Or, Seq(maskedDefault, shiftedValue), Seq.empty, tpe))
+  }
+
   private def onStmt(namespace: Namespace)(stmt: Statement): Statement = stmt match {
     case wire: DefWire => wire.mapType(replaceAndRegister(wire.name))
     case reg: DefRegister => reg.mapExpr(onExpr(namespace)).mapType(replaceAndRegister(reg.name))
     case node @ DefNode(_, name, origValue) =>
       val value = onExpr(namespace)(origValue)
-      replaceAndRegister(name, true, true)(origValue.tpe)
+      replaceAndRegister(name, isInput = true, hasDirection = true)(origValue.tpe)
       if (isReplaced(WRef(name)) && value.tpe != getDefault(WRef(name)).tpe) {
         node.copy(value = vecToUInt(value))
       } else {
@@ -72,60 +107,27 @@ object ReplaceVecOfBools {
     case PartialConnect(info, WSubIndex(vec, index, _, _), origValue) if isReplaced(vec) =>
       val default = getDefault(vec)
       val value = onExpr(namespace)(origValue)
-      val tpe = default.tpe
-
-      val shiftedValue = DoPrim(PrimOps.Shl, Seq(value), Seq(index), default.tpe)
-
-      val UIntType(IntWidth(width)) = default.tpe
-      val oneHot = DoPrim(PrimOps.Shl, Seq(UIntLiteral(1, IntWidth(width))), Seq(index), tpe)
-      val mask = DoPrim(PrimOps.Not, Seq(oneHot), Seq.empty, tpe)
-      val maskedDefault = DoPrim(PrimOps.And, Seq(default, mask), Seq.empty, tpe)
-
-      assignVec(info, namespace, vec, DoPrim(PrimOps.Or, Seq(maskedDefault, shiftedValue), Seq.empty, tpe))
-
-    case Connect(info, WSubIndex(vec, index, _, _), origValue) if isReplaced(vec) =>
-      val default = getDefault(vec)
-      val value = onExpr(namespace)(origValue)
-      val tpe = default.tpe
-
-      val shiftedValue = DoPrim(PrimOps.Shl, Seq(value), Seq(index), default.tpe)
-
-      val UIntType(IntWidth(width)) = default.tpe
-      val oneHot = DoPrim(PrimOps.Shl, Seq(UIntLiteral(1, IntWidth(width))), Seq(index), tpe)
-      val mask = DoPrim(PrimOps.Not, Seq(oneHot), Seq.empty, tpe)
-      val maskedDefault = DoPrim(PrimOps.And, Seq(default, mask), Seq.empty, tpe)
-
-      assignVec(info, namespace, vec, DoPrim(PrimOps.Or, Seq(maskedDefault, shiftedValue), Seq.empty, tpe))
+      replaceIndex(namespace, info, vec, index, default, value, default.tpe)
 
     case PartialConnect(info, WSubAccess(vec, origIndex, _, _), origValue) if isReplaced(vec) =>
       val default = getDefault(vec)
       val value = onExpr(namespace)(origValue)
       val index = onExpr(namespace)(origIndex)
-      val tpe = default.tpe
+      replaceAccess(namespace, info, vec, default, value, index, default.tpe)
 
-      val shiftedValue = DoPrim(PrimOps.Dshl, Seq(value, index), Seq.empty, default.tpe)
+    case PartialConnect(info, expr, value) if isReplaced(expr) =>
+      assignVec(info, namespace, expr, onExpr(namespace)(value))
 
-      val UIntType(IntWidth(width)) = default.tpe
-      val oneHot = DoPrim(PrimOps.Dshl, Seq(UIntLiteral(1, IntWidth(width)), index), Seq.empty, tpe)
-      val mask = DoPrim(PrimOps.Not, Seq(oneHot), Seq.empty, tpe)
-      val maskedDefault = DoPrim(PrimOps.And, Seq(default, mask), Seq.empty, tpe)
-
-      assignVec(info, namespace, vec, DoPrim(PrimOps.Or, Seq(maskedDefault, shiftedValue), Seq.empty, tpe))
+    case Connect(info, WSubIndex(vec, index, _, _), origValue) if isReplaced(vec) =>
+      val default = getDefault(vec)
+      val value = onExpr(namespace)(origValue)
+      replaceIndex(namespace, info, vec, index, default, value, default.tpe)
 
     case Connect(info, WSubAccess(vec, origIndex, _, _), origValue) if isReplaced(vec) =>
       val default = getDefault(vec)
       val value = onExpr(namespace)(origValue)
       val index = onExpr(namespace)(origIndex)
-      val tpe = default.tpe
-
-      val shiftedValue = DoPrim(PrimOps.Dshl, Seq(value, index), Seq.empty, default.tpe)
-
-      val UIntType(IntWidth(width)) = default.tpe
-      val oneHot = DoPrim(PrimOps.Dshl, Seq(UIntLiteral(1, IntWidth(width)), index), Seq.empty, tpe)
-      val mask = DoPrim(PrimOps.Not, Seq(oneHot), Seq.empty, tpe)
-      val maskedDefault = DoPrim(PrimOps.And, Seq(default, mask), Seq.empty, tpe)
-
-      assignVec(info, namespace, vec, DoPrim(PrimOps.Or, Seq(maskedDefault, shiftedValue), Seq.empty, tpe))
+      replaceAccess(namespace, info, vec, default, value, index, default.tpe)
 
     case Connect(info, expr, origValue) if isReplaced(expr) =>
       val value = onExpr(namespace)(origValue)
@@ -141,45 +143,54 @@ object ReplaceVecOfBools {
         i => Connect(info, SubIndex(expr, i, BoolType), onExpr(namespace)(SubIndex(value, i, BoolType))))
       Block(connects)
 
-    case PartialConnect(info, expr, value) if isReplaced(expr) =>
-      assignVec(info, namespace, expr, onExpr(namespace)(value))
-
     case wDefInstance: WDefInstance =>
-      wDefInstance.mapType(replaceAndRegister(wDefInstance.name, true, true))
+      wDefInstance.mapType(replaceAndRegister(wDefInstance.name, isInput = true, hasDirection = true))
 
     case Conditionally(info, origCond, origConseq, origAlt) =>
-      val prevDefaults = replacedVecs.clone()
+      val prevDefaults = candidates.clone()
       val cond = onExpr(namespace)(origCond)
 
       // create new wires for conditional assignments
       val conditionalDefaults = prevDefaults.filter { case (name, _) => !inputs.contains(name) }.map {
-        case (name, _) => (name, WRef(namespace.newTemp, getDefault(keyToExpr(name)).tpe))
+        case (name, _) => (name, (WRef(namespace.newTemp, getDefault(keyToExpr(name)).tpe), false))
       }
 
-      // assign new wires to current default before of conditional
+      // assign new wires to current default before conditional
       val defaults = conditionalDefaults.flatMap {
-        case (name, prevDefault) =>
+        case (name, (prevDefault, _)) =>
           val wire = DefWire(info, prevDefault.name, prevDefault.tpe)
-          Seq(wire, Connect(info, prevDefault, replacedVecs(name)))
+          Seq(wire, Connect(info, prevDefault, candidates(name)._1))
       }.toSeq
 
+      // reset touched flags
+      candidates.foreach{ case (k, (e, _)) => candidates.put(k, (e, false))}
+
       // map conseq with new defaults, reassign newly created wires, then reset defaults
-      val conseq = Block(Seq(onStmt(namespace)(origConseq)) ++ conditionalDefaults.map {
-          case (origName, prevDefault) => Connect(info, prevDefault, replacedVecs(origName))
+      val conseq = Block(Seq(onStmt(namespace)(origConseq)) ++ conditionalDefaults.filter(k => candidates(k._1)._2)
+        .map { case (origName, (prevDefault, _)) =>
+            conditionalDefaults.put(origName, (prevDefault, true))
+            Connect(info, prevDefault, candidates(origName)._1)
         })
-      replacedVecs ++= prevDefaults
+
+      // reset touched flags
+      candidates ++= prevDefaults
+      candidates.foreach{ case (k, (e, _)) => candidates.put(k, (e, false))}
 
       // do the same thing for alt block
       val alt = if (origAlt == EmptyStmt) {
         origAlt
       } else {
-        Block(Seq(onStmt(namespace)(origAlt)) ++ conditionalDefaults.map {
-          case (origName, wRef) => Connect(info, wRef, replacedVecs(origName))
+        Block(Seq(onStmt(namespace)(origAlt)) ++ conditionalDefaults.filter(k => candidates(k._1)._2)
+        .map { case (origName, (prevDefault, _)) =>
+            conditionalDefaults.put(origName, (prevDefault, true))
+            Connect(info, prevDefault, candidates(origName)._1)
         })
       }
-      replacedVecs ++= conditionalDefaults
+      candidates ++= prevDefaults
 
       val conditional = Conditionally(info, cond, conseq, alt)
+      conditionalDefaults.filter(_._2._2).foreach { case (k, (default, _)) => candidates.put(k, (default, true)) }
+
       Block(defaults :+ conditional)
 
     case other => other.mapStmt(onStmt(namespace)).mapExpr(onExpr(namespace))
@@ -187,7 +198,7 @@ object ReplaceVecOfBools {
 
   private def onExpr(namespace: Namespace)(expr: Expression): Expression = expr match {
 
-    case mux @ Mux(origCond, origTval, origFval, tpe) =>
+    case Mux(origCond, origTval, origFval, tpe) =>
       val cond = onExpr(namespace)(origCond)
       val tval = onExpr(namespace)(origTval)
       val fval = onExpr(namespace)(origFval)
@@ -204,16 +215,7 @@ object ReplaceVecOfBools {
       } else {
         Mux(cond, tval, fval, tpe)
       }
-/*
-    case WSubIndex(vec, index, _, _) if isReplaced(vec) =>
-      val wRef = getDefault(vec)
-      DoPrim(PrimOps.Bits, Seq(DoPrim(PrimOps.Shr, Seq(vec), Seq(index), wRef.tpe)), Seq(0, 0), BoolType)
 
-    case WSubAccess(vec, origIndex, _, _) if isReplaced(vec) =>
-      val index = onExpr(namespace)(origIndex)
-      val wRef = getDefault(vec)
-      DoPrim(PrimOps.Bits, Seq(DoPrim(PrimOps.Dshr, Seq(vec, index), Seq.empty, wRef.tpe)), Seq(0, 0), BoolType)
-*/
     case wSubIndex @ WSubIndex(origExpr, index, _, _) =>
       val expr = onExpr(namespace)(origExpr)
       if (expr.tpe != origExpr.tpe) {
@@ -253,17 +255,17 @@ object ReplaceVecOfBools {
 
     val outputDefaults = new mutable.ListBuffer[Statement]()
     val ports = mod.ports.map({ case Port(info, name, direction, origTpe) =>
-      val tpe = replaceAndRegister(name, direction == Input, true)(origTpe)
+      val tpe = replaceAndRegister(name, direction == Input, hasDirection = true)(origTpe)
       Port(info, name, direction, tpe)
     })
 
     val bodyx = onStmt(namespace)(mod.body)
 
-    val finalConnects = replacedVecs.filterKeys(!inputs.contains(_)).map {
-      case (origName, wRef) => Connect(NoInfo, keyToExpr(origName), wRef)
+    val finalConnects = candidates.filterKeys(!inputs.contains(_)).map {
+      case (origName, (wRef, _)) => Connect(NoInfo, keyToExpr(origName), wRef)
     }.toSeq
 
-    replacedVecs.clear()
+    candidates.clear()
     inputs.clear()
 
     mod.copy(ports = ports, body = Block(Block(outputDefaults) +: bodyx +: finalConnects))
@@ -271,7 +273,7 @@ object ReplaceVecOfBools {
 
   private def keyToExpr(name: String): Expression = {
     val path = name.split('.')
-    val bundleRef: Expression = WRef(path.head, replacedVecs(name).tpe)
+    val bundleRef: Expression = WRef(path.head, candidates(name)._1.tpe)
     path.tail.foldLeft(bundleRef)( (expr, field) => WSubField(expr, field) )
   }
 }
@@ -293,7 +295,6 @@ class ReplaceVecOfBools extends Transform {
 
     val circuitState = state.copy(circuit = state.circuit.copy(modules = modulesx))
 
-    println(circuitState.circuit.serialize)
     new ResolveAndCheck().execute(passes.ToWorkingIR.execute(circuitState))
   }
 }
